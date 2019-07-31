ca3f62fa8cd08bf65c3c9306ebfa73d973ba70db
Haoming Jiang <jianghm.ustc@gmail.com>
2019-07-31 14:19:00 -0700

    fix

--- mymodule/sentence_prediction_mtvat.py
+++ mymodule/sentence_prediction_mtvat.py
@@ -135,6 +135,32 @@ class SentencePredictionMTVATTask(SentencePredictionTask):
         if args.regression_target:
             self.args.teacher_class = 'logit'
 
+    @classmethod
+    def setup_task(cls, args, **kwargs):
+        assert args.num_classes > 0, 'Must set --num-classes'
+
+        args.tokens_per_sample = args.max_positions
+
+        # load data dictionary
+        data_dict = cls.load_dictionary(
+            args,
+            os.path.join(args.data, 'input0', 'dict.txt'),
+            source=True,
+        )
+        print('| [input] dictionary: {} types'.format(len(data_dict)))
+
+        label_dict = None
+        if not args.regression_target:
+            # load label dictionary
+            label_dict = cls.load_dictionary(
+                args,
+                os.path.join(args.data, 'label', 'dict.txt'),
+                source=False,
+            )
+            print('| [label] dictionary: {} types'.format(len(label_dict)))
+        else:
+            label_dict = data_dict
+        return cls(args, data_dict, label_dict)
 
     def train_step(self, sample, model, criterion, optimizer, ignore_grad=False):
         """
